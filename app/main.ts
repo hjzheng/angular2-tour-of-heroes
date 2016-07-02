49edd8ea39de733dda0dadaceb55593fe79c4afd
hjzheng <zhenghaoju700@163.com>
2016-07-02 23:11:21 +0800

    Angular2 快速起步

---
+++ app/main.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by hjzheng on 16/7/2.
+ */
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+bootstrap(AppComponent);
